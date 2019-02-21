e44050b9fb8b97f30777a931614001e311f4ff41
Roland <r.guichard@ucl.ac.uk>
2019-02-21 14:42:10 +0000

    Set up a tcp socket in the worker constructor

--- sksurgerybk/interface/bk5000.py
+++ sksurgerybk/interface/bk5000.py
@@ -4,6 +4,12 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, world"
+
+
 #TODO
 class BKMedicalDataSoureActivator():
 
@@ -15,12 +21,23 @@ class BKMedicalDataSoureActivator():
 
 class BKMedicalDataSourceWorker():
 
-    def __init__(timeout, frames_per_second):
+    def __init__(self, timeout, frames_per_second):
+        self.timeout = timeout
+        self.frames_per_second = frames_per_second
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((TCP_IP, TCP_PORT))
+        self.socket.send(MESSAGE)
+        data = self.socket.recv(BUFFER_SIZE)
+        self.socket.close()
+        print "received data: ", data
         pass
-        
+
     def request_stop(self):
         pass
+
     def disconnect_from_host(self):
+        if(self.socket.recv(BUFFER_SIZE)): # If the receive is not empty, disconnect
+            self.socket.close()
         pass
 
     def stop_streaming(self):
@@ -40,14 +57,18 @@ class BKMedicalDataSourceWorker():
 
     def send_command_message(self, message):
         pass
-        
+
 
     def receive_response_message(self, expected_size):
         pass
 
-    
+
     def receive_image(self, image):
         pass
 
     def find_first_a_not_preceded_by_b(self, start_position, buf, a, b):
         pass
+
+if __name__ == '__main__':
+    print "instantiate the class"
+    bkworker = BKMedicalDataSourceWorker(10, 50)
