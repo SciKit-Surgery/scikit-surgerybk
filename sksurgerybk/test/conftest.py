9dfbb6aeeded6b95712a5f7b6013d623ccc70910
Roland <r.guichard@ucl.ac.uk>
2019-02-22 15:44:48 +0000

    A bunch of test fixtures

--- sksurgerybk/test/conftest.py
+++ sksurgerybk/test/conftest.py
@@ -1,10 +1,13 @@
-from pytest import fixture, yield_fixture
+import pytest
 
 import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+timeout = 10. # seconds
+frames_per_second = 50
 
 class TestingServer():
     def __init__(self):
@@ -20,6 +23,18 @@ class TestingServer():
             conn.send(data)  # echo
         conn.close()
 
-@yield_fixture
+@pytest.fixture
+def socket_var():
+    pytest.TCP_IP = TCP_IP
+    pytest.TCP_PORT = TCP_PORT
+    return pytest
+
+@pytest.yield_fixture
 def setup_server():
-    yield TestingServer()
+    return TestingServer()
+
+@pytest.fixture
+def bk_medical_data_source_worker():
+    from sksurgerybk.interface.bk5000 import BKMedicalDataSourceWorker
+    return BKMedicalDataSourceWorker(timeout=timeout,
+                                     frames_per_second=frames_per_second)
