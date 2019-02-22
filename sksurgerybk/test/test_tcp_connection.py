0d772935ea04b33ff6ce876003490cb354e7f0fc
Roland <r.guichard@ucl.ac.uk>
2019-02-22 17:23:57 +0000

    Finalise the test

--- sksurgerybk/test/test_tcp_connection.py
+++ sksurgerybk/test/test_tcp_connection.py
@@ -1,2 +1,11 @@
-def test_TestServer(setup_server):
-    print("Success...")
+
+def test_TestServer(setup_server, bk_medical_data_source_worker, socket_var):
+    test_message = "Test message"
+    bk_medical_data_source_worker.connect_to_host(socket_var.TCP_IP,
+                                                  socket_var.TCP_PORT)
+    bk_medical_data_source_worker.send_command_message(test_message)
+    bk_medical_data_source_worker.receive_response_message(
+                                    socket_var.BUFFER_SIZE)
+    print("Received data in test: {:} \n".format(
+             bk_medical_data_source_worker.data))
+    assert (bk_medical_data_source_worker.data == test_message)
