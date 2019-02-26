8b0398ebe48d451996b7ec59eeb0b16684a7ead9
Roland <r.guichard@ucl.ac.uk>
2019-02-26 10:50:30 +0000

    Issue #1: Define a bytes string test message

--- tests/test_tcp_connection.py
+++ tests/test_tcp_connection.py
@@ -1,6 +1,6 @@
 
 def test_TestServer(setup_server, bk_medical_data_source_worker, socket_var):
-    test_message = "Test message"
+    test_message = b'Test message'
     bk_medical_data_source_worker.connect_to_host(socket_var.TCP_IP,
                                                   socket_var.TCP_PORT)
     bk_medical_data_source_worker.send_command_message(test_message)
