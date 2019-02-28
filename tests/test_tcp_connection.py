59753adf4bfe70f6ccd2d0468fc344cd3cfc10bb
Roland <r.guichard@ucl.ac.uk>
2019-02-28 17:40:13 +0000

    Testing the stop_streaming method

--- tests/test_tcp_connection.py
+++ tests/test_tcp_connection.py
@@ -3,9 +3,24 @@ def test_TestServer(setup_server, bk_medical_data_source_worker, socket_var):
     test_message = b'Test message'
     bk_medical_data_source_worker.connect_to_host(address=socket_var["TCP_IP"],
                                                   port=socket_var["TCP_PORT"])
-    bk_medical_data_source_worker.send_command_message(message=test_message)
-    bk_medical_data_source_worker.receive_response_message(
-                                    expected_size=len(test_message))
+    sentOK = bk_medical_data_source_worker.send_command_message(
+                                           message=test_message)
+
+    assert sentOK == True
+    receiveOK = bk_medical_data_source_worker.receive_response_message(
+                                  expected_size=len(test_message))
+    assert receiveOK == True
     print("Received data in test: {:} \n".format(
              bk_medical_data_source_worker.data))
-    assert (bk_medical_data_source_worker.data == test_message)
+    assert bk_medical_data_source_worker.data == test_message
+
+def test_stop_streaming(setup_server, bk_medical_data_source_worker, socket_var):
+    bk_medical_data_source_worker.connect_to_host(address=socket_var["TCP_IP"],
+                                                  port=socket_var["TCP_PORT"])
+    bk_medical_data_source_worker.stop_streaming()
+    assert bk_medical_data_source_worker.is_streaming == False
+    assert bk_medical_data_source_worker.request_stop_streaming == False
+
+    bk_medical_data_source_worker.receive_response_message()
+    print("Received data in test: {:} \n".format(
+             bk_medical_data_source_worker.data))
