ee77013b5435fa583e0cb460e0c18297b3727cbf
Thomas Dowrick <tdowrick@gmail.com>
2019-03-04 10:23:31 +0000

    Issue #2: PyIGTLink Code

---
+++ bk_pyigtlink.py
@@ -0,0 +1,19 @@
+from work_in_progress import BK
+import logging
+import numpy as np
+
+from pyIGTLink import *
+
+igtlink_server = PyIGTLink(localServer=True)
+
+bk = BK()
+bk.connect()
+
+while True:
+    if igtlink_server.is_connected():
+        bk.get_frame()
+
+        if bk.valid:
+            img_x, img_y = bk.image_size
+            image_message = ImageMessage(bk.result.reshape(img_y, img_x).T)
+            igtlink_server.add_message_to_send_queue(image_message)
