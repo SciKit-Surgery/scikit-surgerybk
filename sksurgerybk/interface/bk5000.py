0c3a423976967e41efdd891fa8fadf4218b75b99
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2019-03-06 09:15:19 +0000

    Issue #1: Fix linting

--- sksurgerybk/interface/bk5000.py
+++ sksurgerybk/interface/bk5000.py
@@ -30,6 +30,7 @@ class BK5000():
         self.socket.settimeout(self.timeout)
         self.request_stop_streaming = False
         self.is_streaming = False
+        self.image_size = [0, 0]
 
     def request_stop(self):
         """Set the appropriate class member"""
@@ -87,11 +88,18 @@ class BK5000():
             self.socket.close()
 
     def query_win_size(self):
+        """ Query the BK5000 for the window/image size """
         query_win_size_message = "QUERY:US_WIN_SIZE;"
         is_ok = self.send_command_message(query_win_size_message)
         if is_ok:
-            response = self.receive_response_message(expected_size = 25)
-            self.parse_win_size_message(self.data.decode())
+            response = self.receive_response_message(expected_size=25)
+            if response:
+                self.parse_win_size_message(self.data.decode())
+            else:
+                print("An error occurred getting the WIN_SIZE response")
+        else:
+            print("An error occured when querying window size")
+        return is_ok
 
     def parse_win_size_message(self, message):
         """Extrack the size of the US window from the response message
@@ -108,17 +116,19 @@ class BK5000():
         self.image_size = [int(s) for s in dim_part_of_message]
 
     def generate_command_message(self, message):
+        #pylint:disable=no-self-use
         """ Append 0x01 and 0x04 to start/end of message before sending
-        
+
         Parameters:
         message(string): the message to be sent"""
         char_start = bytearray.fromhex("01").decode()
-        char_end  = bytearray.fromhex("04").decode()
+        char_end = bytearray.fromhex("04").decode()
 
         message_to_send = char_start + message + char_end
 
         message_length = len(message_to_send)
-        logging.debug("Message to send: {}  Size: {}".format(message_to_send, message_length))
+        logging.debug("Message to send: %s Size: %s", \
+             message_to_send, message_length)
 
         return message_to_send
 
