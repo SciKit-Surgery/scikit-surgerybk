7416e1875a66523341eb771f3993d3c6accf7908
Thomas Dowrick <tdowrick@gmail.com>
2019-03-12 14:02:47 +0000

    Issue #3 Remove TCP Server from tests, use mocking instead

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,58 +8,8 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
-timeout = 10.  # Seconds
+timeout = 2.  # Seconds
 frames_per_second = 50
-
-
-class TestingServer():
-    """The class to define a test server"""
-
-    def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((TCP_IP, TCP_PORT))
-
-    def run(self):
-        self.s.listen(1)
-        conn, addr = self.s.accept()
-        self.conn = conn
-        print("Connection address: {:} \n".format(addr))
-        self.done = False
-        while not self.done:
-            try:
-                data = conn.recv(BUFFER_SIZE)
-                if not data:
-                    break
-                print("Received data in server: {:} \n".format(data))
-                if data == b"\x01QUERY:US_WIN_SIZE;\x04":
-                    conn.send(b"\x01DATA: US_WIN_SIZE 640,480;\x04")
-                else:
-                    conn.send(data)  # echo
-            except Exception:
-                self.done = True
-
-    def close(self):
-        self.done = True
-        self.conn.close()
-
-
-@pytest.fixture
-def socket_var():
-    """A bunch of socket related variables"""
-    socket_var = {"TCP_IP": TCP_IP, "TCP_PORT": TCP_PORT}
-    return socket_var
-
-@pytest.fixture
-def setup_server():
-    """Setting up the server in a thread"""
-    print("Setting up the server \n")
-    test_server = TestingServer()
-    thread = Thread(target=test_server.run)
-    thread.start()
-    yield thread
-    test_server.close()
-    print("The server is now closed.")
-
 
 @pytest.fixture
 def bk_5000():
